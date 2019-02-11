f7fb5888e6baa201e2e6e2fa32258744da25a3ce
nangelos <nicksangelos@gmail.com>
2019-02-11 17:23:30 -0600

    trying to fix this

--- rivalshoops/spiders/rivals.py
+++ rivalshoops/spiders/rivals.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class RivalsSpider(scrapy.Spider):
+class RivalsSpider(CrawlSpider):
     name = 'rivals'
-    allowed_domains = ['rivalshoops.com', 'https://n.rivals.com']
-    start_urls = ['http://rivalshoops.com/',
+    allowed_domains = ['rivalshoops.com', 'n.rivals.com']
+    # allowed_domains = ['www.rivalshoops.com']
+    start_urls = [
     'https://n.rivals.com/prospect_rankings/rivals150/2019',
-    'https://n.rivals.com/prospect_rankings/rivals150/2020',
-    'https://n.rivals.com/prospect_rankings/rivals150/2021']
+    # 'https://n.rivals.com/prospect_rankings/rivals150/2020',
+    # 'https://n.rivals.com/prospect_rankings/rivals150/2021'
+    ]
 
-        rules = (
+    rules = (
         Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
              callback="parse_item",
              follow=True),)
 
     def parse_item(self, response):
-        print('Processing..' + response.url)
+        print('Processing..' + response)
 
     # def parse(self, response):
     #     pass
