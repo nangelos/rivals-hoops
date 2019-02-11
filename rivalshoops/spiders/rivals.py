91ba41fc9520c3516d723e45e64cc269eb2a6511
nangelos <nicksangelos@gmail.com>
2019-02-11 16:13:14 -0600

    [rivals.py] working on rules

--- rivalshoops/spiders/rivals.py
+++ rivalshoops/spiders/rivals.py
@@ -4,8 +4,19 @@ import scrapy
 
 class RivalsSpider(scrapy.Spider):
     name = 'rivals'
-    allowed_domains = ['rivalshoops.com']
-    start_urls = ['http://rivalshoops.com/']
+    allowed_domains = ['rivalshoops.com', 'https://n.rivals.com']
+    start_urls = ['http://rivalshoops.com/',
+    'https://n.rivals.com/prospect_rankings/rivals150/2019',
+    'https://n.rivals.com/prospect_rankings/rivals150/2020',
+    'https://n.rivals.com/prospect_rankings/rivals150/2021']
 
-    def parse(self, response):
-        pass
+        rules = (
+        Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
+             callback="parse_item",
+             follow=True),)
+
+    def parse_item(self, response):
+        print('Processing..' + response.url)
+
+    # def parse(self, response):
+    #     pass
